c9bdaa8624f9d58242bbdd1d7d01c87e6347f8ed
Jonathan Bedon <bedonjonathan@gmail.com>
2021-07-10 14:04:52 -0400

    deployed adn working

--- src/Util/apiURL.js
+++ src/Util/apiURL.js
@@ -3,8 +3,7 @@ export const apiURL = () => {
         process.env.NODE_ENV === 'development' ||
         process.env.NODE_ENV === 'test'
     ) {
-        // return 'http://localhost:3001'
-        return 'https://mega-frugal-app.herokuapp.com'
+        return 'http://localhost:3001'
     }  
     return `https://mega-frugal-app.herokuapp.com`;
 }
